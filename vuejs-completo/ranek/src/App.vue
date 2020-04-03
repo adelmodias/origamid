dc0cac22f18bc1b76bae6c95b28897bea7dc5d2f
Adelmo Dias <adelmodias@outlook.com>
2020-04-02 23:22:20 -0300

    assets and css

--- vuejs-completo/ranek/src/App.vue
+++ vuejs-completo/ranek/src/App.vue
@@ -17,3 +17,59 @@ export default {
   }
 };
 </script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body,
+ul,
+li,
+h1,
+h2,
+p {
+  margin: 0;
+  padding: 0;
+}
+
+ul {
+  list-style: none;
+}
+
+body {
+  font-family: "Avenir", Arial, sans-serif;
+  color: #345;
+  background: url("./assets/pattern.svg") repeat top;
+}
+
+a {
+  color: #345;
+  text-decoration: none;
+}
+
+img {
+  display: block;
+  max-width: 100%;
+}
+
+.btn {
+  display: block;
+  padding: 10px 30px;
+  background: #87f;
+  border-radius: 4px;
+  color: white;
+  text-align: center;
+  font-size: 1rem;
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.2);
+  transition: all 0.3s;
+  border: none;
+  font-family: "Avenir", Arial, sans-serif;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #65d;
+  transform: scale(1.1);
+}
+</style>
--- vuejs-completo/ranek/src/components/TheHeader.vue
+++ vuejs-completo/ranek/src/components/TheHeader.vue
@@ -1,5 +1,12 @@
 <template>
-  <p>header</p>
+  <header>
+    <nav>
+      <router-link to="/" class="logo">
+        <img src="@/assets/ranek.svg" alt="Ranek" />
+      </router-link>
+      <router-link to="/login" class="btn">Vender / Login</router-link>
+    </nav>
+  </header>
 </template>
 
 <script>
