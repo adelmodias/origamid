feba2c6aa4df2ae1f34c834e94b2dbbf932d603b
Adelmo Dias <adelmodias@outlook.com>
2020-04-03 00:31:54 -0300

    working in home

---
+++ vuejs-completo/ranek/src/components/ProductsSearch.vue
@@ -0,0 +1,9 @@
+<template>
+  <section>
+    <!-- <input type="text" /> -->
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
--- vuejs-completo/ranek/src/views/Home.vue
+++ vuejs-completo/ranek/src/views/Home.vue
@@ -1,11 +1,29 @@
 <template>
   <section>
     <h1>Compre ou Venda</h1>
+    <ProductsSearch />
+    <ProductsList />
   </section>
 </template>
 
 <script>
+import ProductsSearch from "@/components/ProductsSearch";
+import ProductsList from "@/components/ProductsList";
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    ProductsSearch,
+    ProductsList
+  }
 };
 </script>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  color: #84f;
+  margin-top: 40px;
+  text-align: center;
+}
+</style>
