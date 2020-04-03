1766eeeeaa6070fcd798416ae7a0bb6802056525
Adelmo Dias <adelmodias@outlook.com>
2020-04-02 22:42:32 -0300

    Workin in header and footer

--- vuejs-completo/ranek/src/App.vue
+++ vuejs-completo/ranek/src/App.vue
@@ -1,5 +1,19 @@
 <template>
   <div id="app">
+    <TheHeader />
     <router-view />
+    <TheFooter />
   </div>
 </template>
+
+<script>
+import TheHeader from "@/components/TheHeader.vue";
+import TheFooter from "@/components/TheFooter.vue";
+
+export default {
+  components: {
+    TheHeader,
+    TheFooter
+  }
+};
+</script>
