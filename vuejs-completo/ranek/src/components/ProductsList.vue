6021e9990c978d75f607006065bbd21acce3af6d
Adelmo Dias <adelmodias@outlook.com>
2020-04-03 20:24:25 -0300

    listing products from api in home page

--- vuejs-completo/ranek/src/components/ProductsList.vue
+++ vuejs-completo/ranek/src/components/ProductsList.vue
@@ -1,7 +1,30 @@
 <template>
-  <div></div>
+  <section class="products-container">
+    <div v-for="product in products" :key="product.id">
+      <img v-if="products.photos" :src="product.photos[0]" :alt="product.photos[0].title" />
+      <h2>{{product.name}}</h2>
+      <p>{{product.price}}</p>
+      <p>{{product.description}}</p>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      products: null
+    };
+  },
+  methods: {
+    getProducts() {
+      fetch("http://localhost:3000/products")
+        .then(response => response.json())
+        .then(response => (this.products = response));
+    }
+  },
+  created() {
+    this.getProducts();
+  }
+};
 </script>
